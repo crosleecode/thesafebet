89f9febe5d14cddb5720e432bf77fc91c8155449
Muqtar Haji <Muktaryare94@gmail.com>
2025-11-25 17:31:27 -0600

    Updated Learn page, removed Privacy, fixed footer, improved homepage

--- obj/Debug/net8.0/SafeBet.AssemblyInfo.cs
+++ obj/Debug/net8.0/SafeBet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SafeBet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52e91fc09b5d66c9948af96fa9738d87dacbdb4a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5e78a2649bec62febb568e05cadb48d883ff5ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("SafeBet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SafeBet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
